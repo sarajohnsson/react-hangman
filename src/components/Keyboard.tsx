d27c00d78d538a9135b857f69027fe45db740e4b
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-05-05 19:40:55 +0200

    Keyboard functionality

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -1,3 +1,64 @@
-export function Keyboard(){
-    
+import '../styles/keyboard.scss';
+
+const KEYS = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'P',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+];
+
+type KeyboardProps = {
+    activeLetters: string[];
+    inactiveLetters: string[];
+    disabled?: boolean;
+    addGuessedLetter: (letter: string) => void;
+};
+
+export function Keyboard({
+    activeLetters,
+    inactiveLetters,
+    disabled = false,
+    addGuessedLetter,
+}: KeyboardProps) {
+    return (
+        <div className="keyboard-wrapper">
+            {KEYS.map((key) => {
+                const isActive = activeLetters.includes(key);
+                const isInactive = inactiveLetters.includes(key);
+                return (
+                    <button
+                        onClick={() => addGuessedLetter(key)}
+                        className={`keyboard-btn ${isActive ? 'active' : ''} ${
+                            isInactive ? 'inactive' : ''
+                        }`}
+                        disabled={isInactive || isActive || disabled}
+                        key={key}>
+                        {key}
+                    </button>
+                );
+            })}
+        </div>
+    );
 }
