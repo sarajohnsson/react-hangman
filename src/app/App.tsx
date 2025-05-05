78275d306708acaf1b9afd39788b38f70b6c34b1
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-05-05 19:41:31 +0200

    Final functionality

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,104 @@
-import { useState } from "react"
-import words from './wordList.json'
-import './style.scss'
-import { HangmanDrawing } from "../components/HangmanDrawing";
-import { HangmanWord } from "../components/HangmanWord";
-import { Keyboard } from "../components/Keyboard";
+import { useCallback, useEffect, useState } from 'react';
+import words from './wordList.json';
+import '../styles/app.scss';
+import { HangmanDrawing } from '../components/HangmanDrawing';
+import { HangmanWord } from '../components/HangmanWord';
+import { Keyboard } from '../components/Keyboard';
 
-function App() {
-  const [wordToGuess, setWordToGuess] = useState(() =>{
+function getWord() {
     return words[Math.floor(Math.random() * words.length)];
-  });
-
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
-
-  console.log(wordToGuess);
-  return (
-    <div className="wrapper">
-      <div className="game-over">
-        Win
-        Lose
-      </div>
-
-      <HangmanDrawing/>
-      <HangmanWord/>
-      <Keyboard />
-    </div>
-  )
 }
 
-export default App
+function App() {
+    const [wordToGuess, setWordToGuess] = useState(getWord);
+
+    const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+    const incorrectLetters = guessedLetters.filter(
+        (letter) => !wordToGuess.includes(letter)
+    );
+
+    const isLoser = incorrectLetters.length >= 6;
+    const isWinner = wordToGuess
+        .split('')
+        .every((letter) => guessedLetters.includes(letter));
+
+    const addGuessedLetter = useCallback(
+        (letter: string) => {
+            if (guessedLetters.includes(letter) || isWinner || isLoser) {
+                return;
+            } else {
+                setGuessedLetters((currentLetters) => [
+                    ...currentLetters,
+                    letter,
+                ]);
+            }
+        },
+        [guessedLetters, isWinner, isLoser]
+    );
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key;
+            if (!key.match(/^[a-z]$/)) {
+                return;
+            } else {
+                e.preventDefault();
+                addGuessedLetter(key);
+            }
+        };
+
+        document.addEventListener('keypress', handler);
+
+        return () => {
+            document.removeEventListener('keypress', handler);
+        };
+    }, [addGuessedLetter, guessedLetters]);
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            const key = e.key;
+            if (key !== 'Enter') {
+                return;
+            } else {
+                e.preventDefault();
+                setGuessedLetters([]);
+                setWordToGuess(getWord);
+            }
+        };
+
+        document.addEventListener('keypress', handler);
+
+        return () => {
+            document.removeEventListener('keypress', handler);
+        };
+    }, []);
+
+    return (
+        <div className="wrapper">
+            <div className="game-over">
+                {isWinner && 'Winner! - Refresh to try again.'}
+                {isLoser && 'Nice Try - Refresh to try again.'}
+            </div>
+
+            <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+            <HangmanWord
+                reveal={isLoser}
+                guessedLetters={guessedLetters}
+                wordToGuess={wordToGuess}
+            />
+            <div style={{ alignSelf: 'stretch' }}>
+                <Keyboard
+                    disabled={isWinner || isLoser}
+                    activeLetters={guessedLetters.filter((letter) =>
+                        wordToGuess.includes(letter)
+                    )}
+                    inactiveLetters={incorrectLetters}
+                    addGuessedLetter={addGuessedLetter}
+                />
+            </div>
+        </div>
+    );
+}
+
+export default App;
