5c65364c8e6019e6a8d0c76c62ba3233b22b3b97
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-05-05 10:07:26 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import words from './wordList.json'
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() =>{
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGUessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
+  return (
+    <h1>Hi</h1>
+  )
+}
+
+export default App
