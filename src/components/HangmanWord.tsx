5a92405557f0e4c75d64497676a014b171726fb3
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-05-05 19:40:16 +0200

    render word

--- src/components/HangmanWord.tsx
+++ src/components/HangmanWord.tsx
@@ -1,7 +1,24 @@
-export function HangmanWord(){
-    return(
-        <div className="word-wrapper">
+import { useState } from 'react';
 
+export function HangmanWord() {
+    const [isVisible, setIsVisible] = useState(false);
+    const word = 'test';
+    const guessedLetters = ['t'];
+
+    return (
+        <div className="word-wrapper">
+            {word.split('').map((letter, index) => (
+                <span className="displayed-word" key={index}>
+                    <span
+                        style={{
+                            visibility: guessedLetters.includes(letter)
+                                ? 'visible'
+                                : 'hidden',
+                        }}>
+                        {letter}
+                    </span>
+                </span>
+            ))}
         </div>
-    )
+    );
 }
