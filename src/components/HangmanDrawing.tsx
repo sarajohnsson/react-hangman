4b71c322d609f47b1a14e5f8a73eb1bfe35b1f72
sarajohnsson <116513890+sarajohnsson@users.noreply.github.com>
2025-05-05 19:40:43 +0200

    Drawing functionality

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,41 +1,31 @@
-const HEAD = (
-    <div className="head"/>
-)
+import '../styles/drawing.scss';
 
-const BODY = (
-    <div className="body"/>
-)
+const HEAD = <div className="head" />;
 
-const RIGHT_ARM = (
-    <div className="right-arm"/>
-)
+const BODY = <div className="body" />;
 
-const LEFT_ARM = (
-    <div className="left-arm"/>
-)
+const RIGHT_ARM = <div className="right-arm" />;
 
-const RIGHT_LEG = (
-    <div className="right-leg"/>
-)
+const LEFT_ARM = <div className="left-arm" />;
 
-const LEFT_LEG = (
-    <div className="left-leg"/>
-)
+const RIGHT_LEG = <div className="right-leg" />;
 
+const LEFT_LEG = <div className="left-leg" />;
 
-export function HangmanDrawing(){
-return (
-    <div className="drawing-wrapper">
-        {HEAD}
-        {BODY}
-        {RIGHT_ARM}
-        {LEFT_ARM}
-        {RIGHT_LEG}
-        {LEFT_LEG}
-        <div className="rope"/>
-        <div className="bar"/>
-        <div className="pillar"/>
-        <div className="base"/>
-    </div>
-)
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+    numberOfGuesses: number;
+};
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+    return (
+        <div className="drawing-wrapper">
+            {BODY_PARTS.slice(0, numberOfGuesses)}
+            <div className="rope" />
+            <div className="bar" />
+            <div className="pillar" />
+            <div className="base" />
+        </div>
+    );
 }
